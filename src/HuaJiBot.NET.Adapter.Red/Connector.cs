6a51fda13327b3999b1521a6f7d47f8771fa5596
LazuliKao <LazuliKao233@outlook.com>
2023-11-04 23:12:16 +0800

    sync

---
+++ src/HuaJiBot.NET.Adapter.Red/Connector.cs
@@ -0,0 +1,10 @@
+﻿namespace HuaJiBot.NET.Adapter.Red;
+
+internal class Connector
+{
+    public void Connect()
+    {
+        Websocket.Client.WebsocketClient client = new("ws://localhost:8080");
+        Console.WriteLine("Connect to Red");
+    }
+}
--- src/HuaJiBot.NET.CLI/App.cs
+++ src/HuaJiBot.NET.CLI/App.cs
@@ -5,6 +5,19 @@ using HuaJiBot.NET.CLI.Config;
 Internal.SetupService(new RedProtocolAdapter()); //协议适配器
 var api = Global.Service; //获取服务实例
 var config = Config.Load(); //配置文件
+Internal.Setup(); //启动
 
 Console.WriteLine("setup");
-Console.ReadLine();
+while (true)
+{
+    var line = Console.ReadLine();
+    var cmds = line.Split(' ');
+    switch (cmds)
+    {
+        case ["quit" or "q"]:
+            break;
+        default:
+            Console.WriteLine($"未知的命令{line}");
+            break;
+    }
+}
