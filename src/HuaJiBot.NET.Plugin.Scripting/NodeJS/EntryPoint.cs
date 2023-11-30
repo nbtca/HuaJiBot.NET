2713502c5e59db7e3d128fee3fce7830c0d9441d
LazuliKao <LazuliKao233@outlook.com>
2023-11-30 22:16:48 +0800

    create Scripting.NodeJS

---
+++ src/HuaJiBot.NET.Plugin.Scripting/NodeJS/EntryPoint.cs
@@ -0,0 +1,4 @@
+﻿using HuaJiBot.NET.Plugin.Scripting;
+
+//标注插件入口点
+[assembly: HuaJiBot.NET.PluginEntryPointAttribute<PluginMain>("Scripting.NodeJS", "NodeJS模块加载器")]
---
+++ src/HuaJiBot.NET.Plugin.Scripting/NodeJS/PluginMain.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Microsoft.JavaScript.NodeApi;
+using Microsoft.JavaScript.NodeApi.Runtime;
+
+namespace HuaJiBot.NET.Plugin.Scripting.NodeJS;
+
+public class PluginMain : PluginBase
+{
+    interface IConsole
+    {
+        void Log(string message);
+    }
+
+    protected override Task Initialize()
+    {
+        var libnodePath = Path.Combine(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
+            "libnode"
+        );
+        var nodejs = new NodejsPlatform(libnodePath).CreateEnvironment();
+        nodejs.Run(() =>
+        {
+            var console = nodejs.Import("global", "console");
+            console.CallMethod("log", "Hello from JS!");
+        });
+        Info("启动成功！");
+        return Task.CompletedTask;
+    }
+
+    protected override void Unload() { }
+}
