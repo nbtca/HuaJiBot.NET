9b70ef92880998e20081444f9f823a674c414279
LazuliKao <LazuliKao233@outlook.com>
2023-12-13 16:53:17 +0800

    头像图片缓存

--- src/HuaJiBot.NET.Plugin.GitHubBridge/Utils/AvatarHelper.cs
+++ src/HuaJiBot.NET.Plugin.GitHubBridge/Utils/AvatarHelper.cs
@@ -1,13 +1,65 @@
-﻿namespace HuaJiBot.NET.Plugin.GitHubBridge.Utils;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HuaJiBot.NET.Plugin.GitHubBridge.Utils;
 
 public static class AvatarHelper
 {
+    private static string GetAvatarFileName(string avatarUrl)
+    { //get md5
+        var hash = MD5.HashData(Encoding.UTF8.GetBytes(avatarUrl));
+        return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant() + ".png";
+    }
+
+    private static string GetAvatarCachePath(string key)
+    {
+        var dir = Path.Combine("cache", "avatar");
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+        return Path.Combine(dir, key);
+    }
+
+    private static bool GetFromCache(string name, [NotNullWhen(true)] out byte[]? bytes)
+    {
+        var path = GetAvatarCachePath(name);
+        if (File.Exists(path))
+        {
+            bytes = File.ReadAllBytes(path);
+            return true;
+        }
+        bytes = null;
+        return false;
+    }
+
+    private static void SaveToCache(string name, byte[] bytes)
+    {
+        var path = GetAvatarCachePath(name);
+        File.WriteAllBytes(path, bytes);
+    }
+
     public static async Task<byte[]> Get(string avatarUrl)
     {
+        var name = GetAvatarFileName(avatarUrl);
+        try
+        {
+            if (GetFromCache(name, out var result))
+            {
+                return result;
+            }
+        }
+        catch
+        {
+            // ignored
+        }
         try
         {
             using HttpClient client = new();
-            return await client.GetByteArrayAsync(avatarUrl);
+            var result = await client.GetByteArrayAsync(avatarUrl);
+            SaveToCache(name, result);
+            return result;
         }
         catch (Exception)
         {
