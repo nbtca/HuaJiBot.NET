16106cb3b5388f8adfc025501830c76ebebac09b
LazuliKao <LazuliKao233@outlook.com>
2025-09-21 23:20:46 +0800

    优化生成器

---
+++ src/HuaJiBot.NET.SourceGenerator/Helper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace HuaJiBot.NET.SourceGenerator;
+
+internal static class Helper
+{
+    public static IEnumerable<INamedTypeSymbol> FilterTypesInNamespace(
+        SemanticModel semanticModel,
+        string namespaceFull
+    )
+    {
+        var namespaceSymbol = semanticModel.Compilation.GlobalNamespace;
+        foreach (var part in namespaceFull.Split('.'))
+        {
+            namespaceSymbol = namespaceSymbol
+                .GetNamespaceMembers()
+                .FirstOrDefault(ns => ns.Name == part);
+            if (namespaceSymbol == null)
+                yield break;
+        }
+        foreach (var type in namespaceSymbol.GetTypeMembers())
+        {
+            yield return type;
+        }
+    }
+
+    public static bool ContainsBaseType(
+        INamedTypeSymbol currentTypeSymbol,
+        INamedTypeSymbol targetTypeSymbol
+    )
+    {
+        var baseType = currentTypeSymbol;
+        while (baseType.BaseType is { } baseNext)
+        {
+            if (baseNext.Equals(targetTypeSymbol, SymbolEqualityComparer.Default))
+                return true;
+            baseType = baseNext;
+        }
+
+        return false;
+    }
+}
